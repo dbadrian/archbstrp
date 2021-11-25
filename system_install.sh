7212d8d7b1137b324ad001af36c811d43ef2adbb
David B. Adrian <dawidh.adrian@gmail.com>
2021-11-25 23:15:04 +0100

    fix variable reference the install device

--- system_install.sh
+++ system_install.sh
@@ -61,7 +61,7 @@ sgdisk --clear \
        --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
        --new=2:0:+${swap_size}GiB   --typecode=2:8200 --change-name=2:swap \
        --new=3:0:0       --typecode=3:8300 --change-name=3:system \
-         $DRIVE
+         ${device}
 
 # EFI
 mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI

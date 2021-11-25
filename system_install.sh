dd25c2be611e3a0edee67d6c34df25a4182cccfb
David B. Adrian <dawidh.adrian@gmail.com>
2021-11-25 23:11:01 +0100

    add some dummy data for faster testing

--- system_install.sh
+++ system_install.sh
@@ -18,20 +18,25 @@ curl -s "$MIRRORLIST_URL" | \
 
 
 ### Get information from user ###
-hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
-clear
-: ${hostname:?"hostname cannot be empty"}
+# hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+# clear
+# : ${hostname:?"hostname cannot be empty"}
 
-user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
-clear
-: ${user:?"user cannot be empty"}
+# user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
+# clear
+# : ${user:?"user cannot be empty"}
 
-password=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
-clear
-: ${password:?"password cannot be empty"}
-password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
-clear
-[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+# password=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
+# clear
+# : ${password:?"password cannot be empty"}
+# password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
+# clear
+# [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+
+
+hostname=testhost
+user=dba
+password=123123
 
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
 device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
